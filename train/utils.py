6692b3e945c766bbecd0c3f59db1834d08f6a4a2
LI949 <76673784+LI949@users.noreply.github.com>
2023-10-04 20:18:41 +0800

    Create utils.py

---
+++ train/utils.py
@@ -0,0 +1,65 @@
+import scanpy as sc
+import numpy as np
+from sklearn import metrics
+from scipy.optimize import linear_sum_assignment
+import torch
+from torch import nn
+
+class ZINBLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, mean, disp, pi, x=None, eps=1e-10):
+        if x is None:
+            zero_nb = torch.pow(disp / (disp + mean + eps), disp)
+            zero_case = -torch.log(pi + ((1.0 - pi) * zero_nb) + eps)
+            return zero_case.mean()
+        #lgamma 计算gamma函数绝对值的自然对数
+        t1 = torch.lgamma(disp + eps) + torch.lgamma(x + 1.0) - torch.lgamma(x + disp + eps)
+        t2 = (disp + x) * torch.log(1.0 + (mean / (disp + eps))) + (x * (torch.log(disp + eps) - torch.log(mean + eps)))
+        nb_final = t1 + t2
+
+        nb_case = nb_final - torch.log(1.0 - pi + eps)
+
+        return nb_case.mean()
+
+# class cluster_loss(nn.Module):
+#
+#     def __init__(self):
+#         super().__init__()
+#
+#     def forward(self, target, pred):
+#         return torch.mean(torch.sum(target * torch.log(target / (pred + 1e-6)), dim=-1))
+        # return torch.sum(target * torch.log(target / (pred + 1e-6)))
+
+def cluster_acc(y_true, y_pred):
+    """
+    Calculate clustering accuracy. Require scikit-learn installed
+    # Arguments
+        y: true labels, numpy.array with shape `(n_samples,)`
+        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+    # Return
+        accuracy, in [0,1]
+    """
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    y_true = y_true.astype(np.float64).astype(np.int64)
+    y_pred = y_pred.astype(np.float64).astype(np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+
+    row_ind,col_ind = linear_sum_assignment(w.max() - w)
+    return w[row_ind, col_ind].sum() * 1.0 / y_pred.size
+
+
+def calculate_metric(pred, label):
+    acc = np.round(cluster_acc(label, pred), 5)
+    nmi = np.round(metrics.normalized_mutual_info_score(label, pred), 5)
+    ari = np.round(metrics.adjusted_rand_score(label, pred), 5)
+
+    return acc, nmi, ari
+
+
